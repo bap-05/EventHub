8903af4838afe827ad4d1721bf6cc84b80b710ed
huyhoangy <huyhoangwf@gmail.com>
2025-12-11 22:27:48 +0700

    update iapi

--- app/src/main/java/com/example/eventhub/API/IAPI.java
+++ app/src/main/java/com/example/eventhub/API/IAPI.java
@@ -1,17 +1,37 @@
 package com.example.eventhub.API;
 
+import com.example.eventhub.Model.SuKien;
+import com.example.eventhub.Model.TaiKhoan;
 import com.example.eventhub.Model.TaiKhoanDN;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface IAPI {
+
     @GET("sukien/")
     Call<ApiResponse> getSK();
+
     @GET("sukien/saptoi")
     Call<ApiResponse> SuKienSapToi();
+
     @POST("taikhoan/")
     Call<ApiResponse> taikhoan(@Body TaiKhoanDN taiKhoanDN);
+
+    @GET("profile/{userId}/sapthamgia")
+    Call<List<SuKien>> getSuKienSapThamGia(@Path("userId") int userId);
+
+    @GET("profile/{userId}/dathamgia")
+    Call<List<SuKien>> getSuKienDaThamGia(@Path("userId") int userId);
+    @GET("profile/profile/{userId}")
+    Call<TaiKhoan> getUserProfile(@Path("userId") int userId);
+
+    // 4. Update avatar (Nếu sau này bạn mở lại tính năng này)
+    // @PUT("profile/update-avatar")
+    // Call<TaiKhoan> updateAvatar(@Body UpdateAvatarRequest request);
 }
--- app/src/main/java/com/example/eventhub/View/Fragment/KhachHang/DaThamGiaFragment.java
+++ app/src/main/java/com/example/eventhub/View/Fragment/KhachHang/DaThamGiaFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.eventhub.API.ApiClient;
+import com.example.eventhub.API.IAPI;
 import com.example.eventhub.Adapter.EventAdapter;
 import com.example.eventhub.Model.SuKien;
 import com.example.eventhub.R;
@@ -18,36 +20,49 @@ import com.example.eventhub.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+
 public class DaThamGiaFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private EventAdapter adapter;
+    private List<SuKien> eventList;
+    private int currentUserId;
+    private static final String ARG_USER_ID = "USER_ID";
+    public  static DaThamGiaFragment newInstance(int userId){
+        DaThamGiaFragment fragment = new DaThamGiaFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_USER_ID,userId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public  void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        if(getArguments()!=null){
+            currentUserId = getArguments().getInt(ARG_USER_ID);
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View view=inflater.inflate(R.layout.fragment_event_list,container,false);
-        RecyclerView recyclerView=view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        List<SuKien> skdathamgia = new ArrayList<>();
-//        skdathamgia.add(new SuKienSapToi(
-//                "Ngày hội tân sinh viên 2025",
-//                "URL_POSTER_3",
-//                "Đã tích lũy",
-//                "12/09 - CN - 7:00",
-//                "Cơ sở 1",
-//                101,
-//                2
-//
-//        ));
-//        skdathamgia.add(new SuKienSapToi(
-//                "Ngày hội tân sinh viên 2025",
-//                "URL_POSTER_4",
-//                "Đã tích lũy",
-//                "12/08 - CN - 7:00",
-//                "Cơ sở 1",
-//                102,
-//                2
-//        ));
-        EventAdapter adapter=new EventAdapter(skdathamgia);
-        recyclerView.setAdapter(adapter);
-        return view;
+        return inflater.inflate(R.layout.fragment_event_list, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        setupRecyclerView();
+        loadEvents();
+    }
+
+    private void loadEvents() {
+        IAPI iapi = ApiClient.getClient().create(IAPI.class);
+//        Call<List<SuKien>> call = iapi.getSuKienDaThamGia(currentUserId);
+
+    }
+
+    private void setupRecyclerView() {
+        
     }
 }
