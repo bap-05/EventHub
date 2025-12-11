368f4c4ea836be04245b0f3bdb04584c1673c204
huyhoangy <huyhoangwf@gmail.com>
2025-12-11 20:54:41 +0700

    update model sukien

--- app/src/main/java/com/example/eventhub/Model/SuKien.java
+++ app/src/main/java/com/example/eventhub/Model/SuKien.java
@@ -1,169 +1,113 @@
 package com.example.eventhub.Model;
 
-import java.util.Date;
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
 
-public class SuKien {
-    String TenSK, TrangThai, ThoiGianBatDau,ThoiGianKetThuc, CoSo, DiaDiem, DiemCong, MoTa,Poster;
-    String SoLuongGioiHan,SoLuongDaDangKy,NguoiDang,NgayTao,AVT1, AVT2, AVT3, AVT4;
-    String MaSK;
+public class SuKien implements Serializable {
 
-    public SuKien( String maSK, String tenSK, String trangThai, String thoiGianBatDau, String thoiGianKetThuc, String coso, String diaDiem, String diemCong, String moTa, String soLuongGioiHan, String soLuongDaDangKy, String nguoiDang, String ngayTao) {
-        TenSK = tenSK;
-        TrangThai = trangThai;
-        ThoiGianBatDau = thoiGianBatDau;
-        ThoiGianKetThuc = thoiGianKetThuc;
-        CoSo = coso;
-        DiaDiem = diaDiem;
-        DiemCong = diemCong;
-        MoTa = moTa;
-        SoLuongGioiHan = soLuongGioiHan;
-        SoLuongDaDangKy = soLuongDaDangKy;
-        NguoiDang = nguoiDang;
-        NgayTao = ngayTao;
-        MaSK = maSK;
+    @SerializedName("MaSK")
+    private int MaSK;
+
+    @SerializedName("TenSK")
+    private String TenSK;
+
+    @SerializedName("Poster")
+    private String Poster;
+
+    @SerializedName("TrangThai")
+    private String TrangThai;
+
+    @SerializedName("ThoiGianBatDau")
+    private String ThoiGianBatDau;
+
+    @SerializedName("ThoiGianKetThuc")
+    private String ThoiGianKetThuc;
+
+    @SerializedName("CoSo")
+    private String CoSo;
+
+    @SerializedName("DiaDiem")
+    private String DiaDiem;
+
+    @SerializedName("DiemCong")
+    private int DiemCong;
+
+    @SerializedName("MoTa")
+    private String MoTa;
+
+    @SerializedName("SoLuongGioiHan")
+    private int SoLuongGioiHan;
+
+    @SerializedName("SoLuongDaDangKy")
+    private int SoLuongDaDangKy;
+
+    @SerializedName("NguoiDang")
+    private int NguoiDang;
+
+    @SerializedName("NgayTao")
+    private String NgayTao;
+
+    @SerializedName("AVT1") private String AVT1;
+    @SerializedName("AVT2") private String AVT2;
+    @SerializedName("AVT3") private String AVT3;
+    @SerializedName("AVT4") private String AVT4;
+
+    public SuKien() {
     }
 
-    public String getPoster() {
-        return Poster;
-    }
 
-    public void setPoster(String poster) {
-        Poster = poster;
-    }
+    public int getMaSK() { return MaSK; }
+    public void setMaSK(int maSK) { MaSK = maSK; }
 
-    public String getTenSK() {
-        return TenSK;
-    }
+    public String getTenSK() { return TenSK; }
+    public void setTenSK(String tenSK) { TenSK = tenSK; }
 
-    public void setTenSK(String tenSK) {
-        TenSK = tenSK;
-    }
+    public String getPoster() { return Poster; }
+    public void setPoster(String poster) { Poster = poster; }
 
-    public String getTrangThai() {
-        return TrangThai;
-    }
+    public String getTrangThai() { return TrangThai; }
+    public void setTrangThai(String trangThai) { TrangThai = trangThai; }
 
-    public void setTrangThai(String trangThai) {
-        TrangThai = trangThai;
-    }
+    public String getThoiGianBatDau() { return ThoiGianBatDau; }
+    public void setThoiGianBatDau(String thoiGianBatDau) { ThoiGianBatDau = thoiGianBatDau; }
 
-    public String getThoiGianBatDau() {
-        return ThoiGianBatDau;
-    }
+    public String getThoiGianKetThuc() { return ThoiGianKetThuc; }
+    public void setThoiGianKetThuc(String thoiGianKetThuc) { ThoiGianKetThuc = thoiGianKetThuc; }
 
-    public void setThoiGianBatDau(String thoiGianBatDau) {
-        ThoiGianBatDau = thoiGianBatDau;
-    }
+    public String getCoSo() { return CoSo; }
+    public void setCoSo(String coSo) { CoSo = coSo; }
 
-    public String getThoiGianKetThuc() {
-        return ThoiGianKetThuc;
-    }
+    public String getDiaDiem() { return DiaDiem; }
+    public void setDiaDiem(String diaDiem) { DiaDiem = diaDiem; }
 
-    public void setThoiGianKetThuc(String thoiGianKetThuc) {
-        ThoiGianKetThuc = thoiGianKetThuc;
-    }
+    public int getDiemCong() { return DiemCong; }
+    public void setDiemCong(int diemCong) { DiemCong = diemCong; }
 
-    public String getCoso() {
-        return CoSo;
-    }
+    public String getMoTa() { return MoTa; }
+    public void setMoTa(String moTa) { MoTa = moTa; }
 
-    public void setCoso(String coso) {
-        CoSo = coso;
-    }
+    public int getSoLuongGioiHan() { return SoLuongGioiHan; }
+    public void setSoLuongGioiHan(int soLuongGioiHan) { SoLuongGioiHan = soLuongGioiHan; }
 
-    public String getDiaDiem() {
-        return DiaDiem;
-    }
+    public int getSoLuongDaDangKy() { return SoLuongDaDangKy; }
+    public void setSoLuongDaDangKy(int soLuongDaDangKy) { SoLuongDaDangKy = soLuongDaDangKy; }
 
-    public void setDiaDiem(String diaDiem) {
-        DiaDiem = diaDiem;
-    }
+    public int getNguoiDang() { return NguoiDang; }
+    public void setNguoiDang(int nguoiDang) { NguoiDang = nguoiDang; }
 
-    public String getDiemCong() {
-        return DiemCong;
-    }
+    public String getNgayTao() { return NgayTao; }
+    public void setNgayTao(String ngayTao) { NgayTao = ngayTao; }
 
-    public void setDiemCong(String diemCong) {
-        DiemCong = diemCong;
-    }
+    // Getters/Setters cho Avatar
+    public String getAVT1() { return AVT1; }
+    public void setAVT1(String AVT1) { this.AVT1 = AVT1; }
 
-    public String getMoTa() {
-        return MoTa;
-    }
+    public String getAVT2() { return AVT2; }
+    public void setAVT2(String AVT2) { this.AVT2 = AVT2; }
 
-    public void setMoTa(String moTa) {
-        MoTa = moTa;
-    }
+    public String getAVT3() { return AVT3; }
+    public void setAVT3(String AVT3) { this.AVT3 = AVT3; }
 
-    public String getSoLuongGioiHan() {
-        return SoLuongGioiHan;
-    }
-
-    public void setSoLuongGioiHan(String soLuongGioiHan) {
-        SoLuongGioiHan = soLuongGioiHan;
-    }
-
-    public String getSoLuongDaDangKy() {
-        return SoLuongDaDangKy;
-    }
-
-    public void setSoLuongDaDangKy(String soLuongDaDangKy) {
-        SoLuongDaDangKy = soLuongDaDangKy;
-    }
-
-    public String getNguoiDang() {
-        return NguoiDang;
-    }
-
-    public void setNguoiDang(String nguoiDang) {
-        NguoiDang = nguoiDang;
-    }
-
-    public String getNgayTao() {
-        return NgayTao;
-    }
-
-    public void setNgayTao(String ngayTao) {
-        NgayTao = ngayTao;
-    }
-
-    public String getAVT1() {
-        return AVT1;
-    }
-
-    public void setAVT1(String AVT1) {
-        this.AVT1 = AVT1;
-    }
-
-    public String getAVT2() {
-        return AVT2;
-    }
-
-    public void setAVT2(String AVT2) {
-        this.AVT2 = AVT2;
-    }
-
-    public String getAVT3() {
-        return AVT3;
-    }
-
-    public void setAVT3(String AVT3) {
-        this.AVT3 = AVT3;
-    }
-
-    public String getAVT4() {
-        return AVT4;
-    }
-
-    public void setAVT4(String AVT4) {
-        this.AVT4 = AVT4;
-    }
-
-    public String getMaSK() {
-        return MaSK;
-    }
-
-    public void setMaSK(String maSK) {
-        MaSK = maSK;
-    }
+    public String getAVT4() { return AVT4; }
+    public void setAVT4(String AVT4) { this.AVT4 = AVT4; }
 }
