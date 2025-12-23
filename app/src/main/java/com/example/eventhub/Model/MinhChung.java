718b6d63dd541516d8411d0afdd21acde1963c9e
Nam <hoainamn488@gmail.com>
2025-12-23 22:07:39 +0700

    update luu vi tri cho chuc nang UPLoadMinhChung

--- app/src/main/java/com/example/eventhub/Model/MinhChung.java
+++ app/src/main/java/com/example/eventhub/Model/MinhChung.java
@@ -3,9 +3,11 @@ package com.example.eventhub.Model;
 public class MinhChung {
     private int MaSK;
     private String AnhMinhChung;
+    private String DiaDiemMinhChung;
 
-    public MinhChung(int maSK, String anhMinhChung) {
+    public MinhChung(int maSK, String anhMinhChung, String diaDiemMinhChung) {
         MaSK = maSK;
         AnhMinhChung = anhMinhChung;
+        DiaDiemMinhChung = diaDiemMinhChung;
     }
 }
